1511733d2ff4a1e89f5b2a4011918714a338e04b
kevinpallado <krbp.97@gmail.com>
2019-11-21 07:02:31 +0800

    Change socket-io to web socket native

--- server.js
+++ server.js
@@ -1,13 +1,38 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mysqlConnection = require('./connection');
+const express = require('express'),
+    bodyParser = require('body-parser'),
+    http = require('http'),
+    routes = require('./routes/routes'),
+    crypto = require('crypto'),
+    static = require('node-static'),
+    file = new static.Server('./'),
+    app = express(),
+    port = 3210;
 
-const routes = require('./routes/routes');
+app.listen(3000);
 
-
-var app = express();
 app.use(bodyParser.json());
 
 app.use('/automation', routes);
 
-app.listen(3000);
+const server = http.createServer((req, res) => {
+    req.addListener('end', () => file.serve(req, res)).resume();
+  });
+
+server.listen(port, () => {
+    console.log(`Websocket Server running at http://localhost:${port}`);
+});
+
+server.on('upgrade', function(req, socket) {
+    if(req.headers['upgrade'] !== 'websocket') {
+        socket.end('HTTP/1.1 400 Bad Request');
+        return;
+    }
+});
+
+function generateAcceptValue (acceptKey) 
+{
+    return crypto
+    .createHash('sha1')
+    .update(acceptKey + '258EAFA5-E914–47DA-95CA-C5AB0DC85B11', 'binary')
+    .digest('base64');
+}
