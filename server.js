97bbb00e571f8df9f1e2f46730fa183651a0d6f5
kevinpallado <krbp.97@gmail.com>
2020-03-01 20:28:01 +0800

    Welcome page

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ require('dns').lookup(require('os').hostname(), function (err, add, fam) {
 
 app.use('/automation', routes.Router);
 app.get('/', function (req, res) {
-    res.sendFile('client/index.html');
+    res.sendFile(__dirname + 'client/index.html');
 });
 
 
